0ec843352fbcad151cc3f0717b4d241f39c3840a
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-02 15:12:33 +0800

    create school.java

---
+++ src/school/management/system/School.java
@@ -0,0 +1,10 @@
+package school.management.system;
+
+public class School {
+	
+	private Teacher teacher;
+	private Student student;
+	private int totalMoneyEarned;
+	private int totalMoneySpent;
+	
+}
