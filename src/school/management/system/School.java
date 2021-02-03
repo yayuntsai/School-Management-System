cf66c196aa657ac1fb6b69f049ff76bb06d0f128
anniesnoopymd <anniesnoopymd@gmail.com>
2021-02-03 14:37:03 +0800

    make updateTotalMoneySpent method static

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -1,10 +1,64 @@
 package school.management.system;
 
+import java.util.List;
+
 public class School {
 	
-	private Teacher teacher;
-	private Student student;
+//	private Teacher teacher;
+//	private Student student;
+//	private static int totalMoneyEarned;
+//	private static int totalMoneySpent;
+	
+
+	private List<Teacher> teachers;
+	private List<Student> students;
 	private int totalMoneyEarned;
 	private int totalMoneySpent;
 	
+	
+	public School(List<Teacher> teachers, List<Student> students) {
+		this.teachers = teachers;
+		this.students = students;
+		totalMoneyEarned = 0;
+		totalMoneySpent = 0;
+	}
+	
+	public List<Teacher> getTeachers(){
+		return teachers;
+	}
+	
+	public void addTeachers(Teacher teacher) {
+		teachers.add(teacher);
+	}
+	
+	/*
+	 * Return the list of students in the school
+	 */
+	public List<Student> getStudents(){
+		return students;
+	}
+	
+	/*
+	 * Add a student to the school
+	 */
+	public void addStudents(Student student) {
+		students.add(student);
+	}
+	
+	public int getTotalMoneyEarned() {
+		return totalMoneyEarned;
+	}
+	
+	public void static updateTotalMoneyEarned(int moneyEarned) {
+		totalMoneyEarned += moneyEarned;
+	}
+	
+	public int getTotalMoneySpent() {
+		return totalMoneySpent;
+	}
+	 
+	public void static updateTotalMoneySpent(int moneySpent) {
+		totalMoneySpent += moneySpent;
+	}
 }
+ 
